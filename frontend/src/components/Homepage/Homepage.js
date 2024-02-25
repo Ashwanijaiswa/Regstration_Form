6fc563e5085138123828cd741fc9e9d04865d52a
ASHWANI JAISWAL <159615991+Ashwanijaiswa@users.noreply.github.com>
2024-02-25 13:46:50 +0530

    Update Homepage.js

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -4,7 +4,7 @@ import "./Homepage.css"
 const Homepage = ({setLoginUser}) => {
     return (
         <div className="homepage">
-            <h2>Boomm... Guy's, I'm Ashutosh.</h2>
+            <h2>Boomm... Guy's, I'm Ashwani.</h2>
             <div className="button" onClick={() => setLoginUser({})} >Logout</div>
         </div>
     )
